756ddd07cc2d983ac686f091c66dda8cc1d49825
Poh Kah Kong <kahkong87mail@gmail.com>
2018-06-29 23:16:12 +0800

    Add ActorHierarchyExperiments

---
+++ src/main/java/com/lightbend/akka/sample/ActorHierarchyExperiments2.java
@@ -0,0 +1,64 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+class StartStopActor1 extends AbstractActor {
+    @Override
+    public void preStart() {
+        System.out.println("first started");
+        getContext().actorOf(Props.create(StartStopActor2.class), "second");
+    }
+
+    @Override
+    public void postStop() {
+        System.out.println("first stopped");
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("stop", s -> {
+                    getContext().stop(getSelf());
+                })
+                .build();
+    }
+}
+
+class StartStopActor2 extends AbstractActor {
+    @Override
+    public void preStart() {
+        System.out.println("second started");
+    }
+
+    @Override
+    public void postStop() {
+        System.out.println("second stopped");
+    }
+
+    // Actor.emptyBehavior is a useful placeholder when we don't
+    // want to handle any messages in the actor.
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .build();
+    }
+}
+
+public class ActorHierarchyExperiments2 {
+    public static void main(String[] args) throws java.io.IOException {
+        ActorSystem system = ActorSystem.create("testSystem");
+
+        ActorRef first = system.actorOf(Props.create(StartStopActor1.class), "first");
+        first.tell("stop", ActorRef.noSender());
+
+        System.out.println(">>> Press ENTER to exit <<<");
+        try {
+            System.in.read();
+        } finally {
+            system.terminate();
+        }
+    }
+}
---
+++ src/main/java/com/lightbend/akka/sample/ActorHierarchyExperiments3.java
@@ -0,0 +1,57 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+class SupervisingActor extends AbstractActor {
+    ActorRef child = getContext().actorOf(Props.create(SupervisedActor.class), "supervised-actor");
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("failChild", f -> {
+                    child.tell("fail", getSelf());
+                })
+                .build();
+    }
+}
+
+class SupervisedActor extends AbstractActor {
+    @Override
+    public void preStart() {
+        System.out.println("supervised actor started");
+    }
+
+    @Override
+    public void postStop() {
+        System.out.println("supervised actor stopped");
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("fail", f -> {
+                    System.out.println("supervised actor fails now");
+                    throw new Exception("I failed!");
+                })
+                .build();
+    }
+}
+
+public class ActorHierarchyExperiments3 {
+    public static void main(String[] args) throws java.io.IOException {
+        ActorSystem system = ActorSystem.create("testSystem");
+
+        ActorRef supervisingActor = system.actorOf(Props.create(SupervisingActor.class), "supervising-actor");
+        supervisingActor.tell("failChild", ActorRef.noSender());
+
+        System.out.println(">>> Press ENTER to exit <<<");
+        try {
+            System.in.read();
+        } finally {
+            system.terminate();
+        }
+    }
+}
